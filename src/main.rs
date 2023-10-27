072bb9ffb40ff90ebd0231b035b6bba79dc810e8
greym0uth <thejadenjack@gmail.com>
2023-10-26 23:44:43 -0500

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
-use esp_idf_svc::hal::gpio::PinDriver;
-use esp_idf_svc::hal::prelude::Peripherals;
-use esp_idf_svc::hal::spi::config::{Config, DriverConfig};
-use esp_idf_svc::hal::spi::{SpiDriver, SpiSharedDeviceDriver};
+mod block;
+
+use embedded_sdmmc::{Volume, VolumeIdx, VolumeManager};
+use esp_idf_hal::{
+    delay::{Delay, Ets},
+    gpio::PinDriver,
+    prelude::Peripherals,
+    spi::{
+        config::{Config, DriverConfig, Mode, MODE_3},
+        Spi, SpiBusDriver, SpiDeviceDriver, SpiDriver, SpiDriverConfig, SpiSharedDeviceDriver,
+        SpiSingleDeviceDriver, SPI2,
+    },
+    units::MegaHertz,
+};
+// use esp_idf_svc::hal::spi::{SpiDriver, SpiSharedDeviceDriver};
 use esp_idf_svc::hal::units::Hertz;
+
+use crate::block::Clock;
 
 fn main() -> anyhow::Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -22,20 +35,30 @@ fn main() -> anyhow::Result<()> {
     let mut reset = PinDriver::output(peripherals.pins.gpio8)?;
     let dreq = PinDriver::input(peripherals.pins.gpio10)?;
 
-    let device = SpiDriver::new(peripherals.spi2, sclk, sdo, sdi, &DriverConfig::default())?;
+    let device = SpiDriver::new::<SPI2>(
+        peripherals.spi2,
+        sclk,
+        sdo,
+        sdi,
+        &SpiDriverConfig::default(),
+    )?;
 
-    let mut cs = PinDriver::output(peripherals.pins.gpio6)?;
-    let ctrl_config = Config::default().baudrate(Hertz(250000));
-    let ctrl_driver = SpiSharedDeviceDriver::new(&device, &ctrl_config)?;
+    // let mut cs = PinDriver::output(peripherals.pins.gpio6)?;
+    // let ctrl_config = Config::default().baudrate(Hertz(250000));
+    // let ctrl_driver = SpiSharedDeviceDriver::new(&device, &ctrl_config)?;
 
     let mut card_cs = PinDriver::output(peripherals.pins.gpio0)?;
-    let data_config = Config::default().baudrate(Hertz(8000000));
-    let data_driver = SpiSharedDeviceDriver::new(&device, &data_config);
+    let data_config = Config::default().baudrate(MegaHertz(12).into());
+    let data_driver = SpiSharedDeviceDriver::new(&device, &data_config)?;
+    // let data_driver = SpiDeviceDriver::new(device, Some(card_cs), &data_config);
+    // let data_driver = SpiBusDriver::new(device, &data_config)?;
+
+    // let data_driver = SpiDeviceDriver::new(&device, None, &data_config)?;
 
     // Setup chip selects
     {
         reset.set_low()?;
-        cs.set_high()?;
+        // cs.set_high()?;
         card_cs.set_high()?;
     }
 
@@ -45,14 +68,30 @@ fn main() -> anyhow::Result<()> {
         reset.set_high()?;
     }
 
-    // Try reading then writing shit
-    // {
-    //     ctrl_driver.lock(|driver| {
-    //         driver.write(&[]).unwrap();
-    //         let mut data: &mut [u8] = &mut [];
-    //         driver.read(&mut data).unwrap();
-    //     });
-    // }
+    log::info!("Locking driver");
+    data_driver.lock(|driver| {
+        let sd = embedded_sdmmc::SdCard::new(driver, card_cs, Ets);
+        log::info!("Card opened ({} bytes)", sd.num_bytes().unwrap());
+        let mut volume_mgr = VolumeManager::new(sd, Clock);
+        log::info!("Created Volume Manager");
+        let volume = volume_mgr.open_raw_volume(VolumeIdx(0)).unwrap();
+        log::info!("Opened Volume {:?}", volume);
+        let root_dir = volume_mgr.open_root_dir(volume).unwrap();
+        let file = volume_mgr
+            .open_file_in_dir(root_dir, "track001.mp3", embedded_sdmmc::Mode::ReadOnly)
+            .unwrap();
+        log::info!("Opened file!");
+        // while !volume_mgr.file_eof(file).unwrap() {
+        //     let mut buffer = [0u8; 32];
+        //     let num_read = volume_mgr.read(file, &mut buffer).unwrap();
+        //     for b in &buffer[0..num_read] {
+        //         print!("{}", *b as char);
+        //     }
+        // }
+
+        volume_mgr.close_file(file).unwrap();
+        volume_mgr.close_dir(root_dir).unwrap();
+    });
 
     log::info!("Hello, world!");
     Ok(())
